2db1d91d59ae9683890b540f38b2d84da779723a
loganathan94 <logiraj359@gmail.com>
2021-09-09 23:19:17 -0700

    sep10project

---
+++ src/test/java/class1.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class class1 {
+
+	
+	@Test
+	private void test1() {
+		
+		
+		System.out.println("test1");
+		System.out.println("test2");
+		
+	}
+}
